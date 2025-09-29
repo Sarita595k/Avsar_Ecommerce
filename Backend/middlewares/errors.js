dcfdb7d1e96ea7fe2017444a4421625465beca5f
sarita595k <sarita595k@gmail.com>
2025-09-29 22:36:34 +0530

    small commit

--- Backend/middlewares/errors.js
+++ Backend/middlewares/errors.js
@@ -3,6 +3,7 @@ export const handleError = (err, req, res, next) => {
     err.statusCode = err.statusCode || 500;
     // err.message = err.message || "internal server error"
 
+    // to manage all the errors 
     if (process.env.NODE_ENV === "development") {
         res.status(err.statusCode).json({
             success: false,
