6196b306d51c0f07f4cdcab3c4350fda73b00677
sarita595k <sarita595k@gmail.com>
2025-11-13 14:30:06 +0530

    pagination added in backend

--- Backend/controllers/productController.js
+++ Backend/controllers/productController.js
@@ -17,14 +17,22 @@ export const newProduct = async (req, res, next) => {
 // get all products /api/products?keywords="apple"
 export const getProducts = async (req, res, next) => {
 
+    const resPerPage = 5;
+
+    // used in frontend to check how many products are there
+    const productCount = await Product.countDocuments()
+
     const apiFeatures = new ApiFeatures(Product.find(), req.query)
         .search()
         .filter()
+        .pagination(resPerPage)
 
     const products = await apiFeatures.query;
+
     res.status(200).json({
         success: true,
         count: products.length,
+        productCount,
         products
     })
 }
