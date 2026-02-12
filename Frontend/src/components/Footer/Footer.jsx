e7548638c788023a328a8486da82329c1797e59b
sarita595k <sarita595k@gmail.com>
2026-02-12 11:00:51 +0530

    small change

--- Frontend/src/components/Footer/Footer.jsx
+++ Frontend/src/components/Footer/Footer.jsx
@@ -1,8 +1,12 @@
+import styled from "styled-components"
+const Div = styled.div`
+bottom:0;
+`
 const Footer = () => {
     return (
-        <>
+        <Div>
             <h5>all rights reserved</h5>
-        </>
+        </Div>
     );
 };
 
